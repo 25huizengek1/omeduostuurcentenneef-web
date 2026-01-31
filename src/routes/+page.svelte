b029d987135080357c4f49838b59ce27f9ba1e13
Bart Oostveen <bart.oos.2006@gmail.com>
2026-01-31 23:34:20 +0100

    chore: update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,68 +4,39 @@
 
   import GitHubIcon from "@lucide/svelte/icons/github";
   import EmailIcon from "@lucide/svelte/icons/mail";
-  import FileIcon from "@lucide/svelte/icons/file";
-  import EthernetPortIcon from "@lucide/svelte/icons/ethernet-port";
-  import LinkButton from "../components/LinkButton.svelte";
+  import ChatIcon from "@lucide/svelte/icons/message-circle";
 </script>
 
 <main
   class="prose justify-self-center-safe p-2 py-12 prose-zinc md:prose-lg lg:prose-xl dark:prose-invert"
 >
-  <h1 class="justify-self-center">Bart Oostveen</h1>
+  <h1 class="justify-self-center text-center">Bart Oostveen's placeholder</h1>
 
-  <div class="flex gap-4 justify-self-center-safe">
-    <a href="https://github.com/25huizengek1"
-      ><Button class="cursor-pointer" variant="outline"><GitHubIcon /></Button></a
-    >
-    <a href="mailto:bart@bartoostveen.nl" rel="nofollow"
-      ><Button class="cursor-pointer" variant="outline"><EmailIcon /></Button></a
-    >
+  <div class="flex gap-4 justify-self-center">
+    <a href="https://github.com/25huizengek1">
+      <Button class="cursor-pointer" variant="outline"><GitHubIcon /></Button>
+    </a>
+    <a href="mailto:bart@bartoostveen.nl" rel="nofollow">
+      <Button class="cursor-pointer" variant="outline"><EmailIcon /></Button>
+    </a>
+    <a href="https://matrix.to/#/@bartoostveen:matrix.org">
+      <Button class="cursor-pointer" variant="outline"><ChatIcon /></Button>
+    </a>
   </div>
 
   <p class="lead">
     Hi, my name is Bart Oostveen. If you want to contact me, you should use one of the above links.
-    Anyway, the rest of this page is Dutch nonsense. Enjoy!
   </p>
-
-  <p>Hoe dan ook, op deze server (of nou ja server, gare Hetzner box) staan een aantal services:</p>
-
-  <div class="flex flex-wrap justify-center gap-2">
-    <LinkButton href="https://fs.omeduostuurcentenneef.nl">
-      {#snippet icon()}
-        <FileIcon />
-      {/snippet}
-      Omeduoparty
-    </LinkButton>
-    <LinkButton href="https://headscale.vitune.app">
-      {#snippet icon()}
-        <EthernetPortIcon />
-      {/snippet}
-      Headscale
-    </LinkButton>
-    <LinkButton href="https://webmail.bartoostveen.nl">
-      {#snippet icon()}
-        <EmailIcon />
-      {/snippet}
-      Webmail
-    </LinkButton>
-  </div>
 
   <p>
-    <i
-      >Oja, en deze server heeft een aantal vitune.app domeinen, want <a
-        href="https://github.com/25huizengek1/ViTune">ViTune</a
-      > is een oud project waarvoor sommige mensen mij nog steeds proberen te bereiken op development@vitune.app</i
-    >
-  </p>
+    You might be coming from ViTune, in that case, if you want to contact me, use
 
-  <p>Mocht je nu nog denken, wie is deze staatsmongool die deze boel in de lucht houdt? Nou...</p>
-  <LinkButton href="https://github.com/25huizengek1">
-    {#snippet icon()}
-      <GitHubIcon />
-    {/snippet}
-    Bart Oostveen
-  </LinkButton>
+    <a href="mailto:development@vitune.app" rel="nofollow">
+      <Button class="cursor-pointer" variant="outline"><EmailIcon />this email</Button>
+    </a>
+
+    instead.
+  </p>
 </main>
 
 <div class="fixed top-0 justify-self-end p-4">
